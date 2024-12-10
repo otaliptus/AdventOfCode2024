02788aeceaac4e7c4ebf0017897883b2358663f1
otaliptus <omertalib42@gmail.com>
2024-12-10 12:41:54 +0100

    Add day10

---
+++ day10/README.md
@@ -0,0 +1,3 @@
+### Day 10
+
+Nothing more than a simple DFS, time to sleep a bit in the morning.
---
+++ day10/day10_part1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+int n, m, res;
+
+std::vector< std::vector<int> > dir = {
+    {-1, 0},
+    {1, 0},
+    {0, -1},
+    {0, 1}
+};
+
+std::set< std::pair<int, int> > st;
+
+void dfs(const std::vector< std::string > &grid, int x, int y, char num) {
+    if(num == '9') {
+        st.insert({x, y});
+        return;
+    }
+
+    for(auto it : dir) {
+        int dx = x + it[0], dy = y + it[1];
+        if(dx < 0 || dx >= n || dy < 0 || dy >= m) {
+            continue;
+        }
+
+        if(grid[dx][dy] - grid[x][y] == 1) {
+            dfs(grid, dx, dy, grid[dx][dy]);
+        }
+    }
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    std::freopen("input.txt", "r", stdin);
+
+    std::vector< std::string > grid;
+    std::string line;
+
+    while(std::getline(std::cin, line)) {
+        grid.push_back(line);
+    }
+
+    n = grid.size();
+    m = grid[0].size();
+
+    for(int i = 0;n > i;i++) {
+        for(int j = 0;m > j;j++) {
+            if(grid[i][j] == '0') {
+                dfs(grid, i, j, '0');
+                res += st.size();
+                st.clear();
+            }
+        }
+    }
+
+    std::cout << res;
+}
